a0567e6ac140b510a6f29b962c968c868a9bad0c
alefew <alefewyimer4@gmail.com>
2024-02-27 21:51:07 +0300

    add chi router platform

---
+++ subscription-service/platforms/routers/routers.go
@@ -0,0 +1,38 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+// Route data will be registered to http listener
+type Route struct {
+	Method      string
+	Path        string
+	Handle      http.HandlerFunc
+	MiddleWares []http.HandlerFunc
+}
+
+func Routes(routes []Route) http.Handler {
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	for _, route := range routes {
+		mux.Method(route.Method, route.Path, route.Handle)
+	}
+	return mux
+}
