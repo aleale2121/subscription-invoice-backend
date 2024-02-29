5478ed0480d2432eb33598c909c18f961eb24d03
alefew <alefewyimer4@gmail.com>
2024-02-29 15:39:17 +0300

    fix formatting

--- subscription-service/internal/constants/states/states.go
+++ subscription-service/internal/constants/states/states.go
@@ -1,9 +1,9 @@
 package states
 
 const (
-	PENDING     = "PENDING-PAYMENT"
-	ACTIVE      = "ACTIVE"
-	INACTIVE    = "INACTIVE"
-	TIME_LAYOUT = "2006-01-02"
+	PENDING         = "PENDING-PAYMENT"
+	ACTIVE          = "ACTIVE"
+	INACTIVE        = "INACTIVE"
+	TIME_LAYOUT     = "2006-01-02"
 	MAX_EMAIL_RETRY = 5
 )
